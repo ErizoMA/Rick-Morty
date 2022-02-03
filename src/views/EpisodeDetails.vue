204304262d9475653833c1eeb6abcda590cef1af
Erick Marcelo <marcerican@gmail.com>
2022-02-02 23:09:46 -0500

    Show all characters that appears in a episode

--- src/views/EpisodeDetails.vue
+++ src/views/EpisodeDetails.vue
@@ -1,30 +1,29 @@
 <template>
   <div v-if="details" class="container">
-    <p>{{ details.name }}</p>
+    <p class="title">{{ details.name }}</p>
     <div class="info">
       <div class="info__episode">
-        <p>Episode</p>
-        <p>{{ details.episode }}</p>
+        <p class="info__title">Episode</p>
+        <p class="info__subtitle">{{ details.episode }}</p>
       </div>
       <div class="info__date">
-        <p>Date</p>
-        <p>{{ details.air_date }}</p>
+        <p class="info__title">Date</p>
+        <p class="info__subtitle">{{ details.air_date }}</p>
       </div>
     </div>
-    <div class="cast">
-      <p>Cast</p>
-      <p v-for="character in details.characters" :key="character">
-        {{ character }}
-      </p>
-    </div>
+    <p class="subtitle">Cast</p>
+    <CardsContainer v-if="details" :residents="details.characters" />
   </div>
 </template>
 
 <script>
 import { APISettings } from "../api/config";
 import getDetails from "../composables/getDetails";
+import CardsContainer from "../components/CardsContainer.vue";
+
 export default {
   props: ["id"],
+  components: { CardsContainer },
   setup(props) {
     const { episodesUrl } = APISettings;
     const { details, fetchData } = getDetails(props, episodesUrl);
@@ -34,14 +33,48 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.title {
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+  color: #081f32;
+  margin: 30px 0 20px 0;
+}
 .info {
   display: flex;
+  gap: 224px;
+  width: fit-content;
+  margin: 0 auto;
 }
-.info__episode {
-  flex: 1;
+.info__title {
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+  color: #081f32;
 }
-.info__date {
-  flex: 1;
+.info__subtitle {
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  color: #6e798c;
+}
+
+.subtitle {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+  color: #8e8e93;
+  text-align: left;
+  margin-bottom: 24px;
+}
+.cards-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  place-items: center;
+  row-gap: 24px;
 }
 </style>
