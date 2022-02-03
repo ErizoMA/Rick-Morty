7cba6fdac96a07825f675bd6c0bcd35fee5d8986
Erick Marcelo <marcerican@gmail.com>
2022-02-02 22:56:25 -0500

    Show all residents of a Location

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div v-if="residents" class="cards-container">
+    <CharacterCard
+      v-for="resident in arrayData"
+      :key="resident.id"
+      :character="resident"
+    />
+  </div>
+</template>
+
+<script>
+import getAllResults from "../composables/getAllResults";
+import CharacterCard from "../components/CharacterCard.vue";
+
+export default {
+  props: ["residents"],
+  components: { CharacterCard },
+  setup(props) {
+    const { fetchArrayUrls, arrayData } = getAllResults(props.residents);
+    fetchArrayUrls();
+    return { arrayData };
+  },
+};
+</script>
+
+<style scoped>
+.cards-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  place-items: center;
+  row-gap: 24px;
+}
+</style>
--- src/views/LocationDetails.vue
+++ src/views/LocationDetails.vue
@@ -1,32 +1,28 @@
 <template>
-  <div class="container">
-    <p>{{ details.name }}</p>
+  <div v-if="details" class="container">
+    <p class="title">{{ details.name }}</p>
     <div class="info">
-      <div class="info__episode">
-        <p>Type</p>
-        <p>{{ details.type }}</p>
-      </div>
-      <div class="info__date">
-        <p>Dimension</p>
-        <p>{{ details.dimension }}</p>
-      </div>
-    </div>
-    <div>
-      <p>Residents</p>
       <div>
-        <p v-for="resident in details.residents" :key="resident">
-          {{ resident }}
-        </p>
+        <p class="info__title">Type</p>
+        <p class="info__subtitle">{{ details.type }}</p>
+      </div>
+      <div>
+        <p class="info__title">Dimension</p>
+        <p class="info__subtitle">{{ details.dimension }}</p>
       </div>
     </div>
+    <p class="subtitle">Residents</p>
+    <CardsContainer v-if="details" :residents="details.residents" />
   </div>
 </template>
 
 <script>
 import { APISettings } from "../api/config";
 import getDetails from "../composables/getDetails";
+import CardsContainer from "../components/CardsContainer.vue";
 export default {
   props: ["id"],
+  components: { CardsContainer },
   setup(props) {
     const { locationsUrl } = APISettings;
     const { details, fetchData } = getDetails(props, locationsUrl);
@@ -36,4 +32,48 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.title {
+  font-size: 36px;
+  line-height: 42px;
+  text-align: center;
+  color: #081f32;
+  margin: 30px 0 20px 0;
+}
+.info {
+  display: flex;
+  gap: 224px;
+  width: fit-content;
+  margin: 0 auto;
+}
+.info__title {
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+  color: #081f32;
+}
+.info__subtitle {
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  color: #6e798c;
+}
+
+.subtitle {
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  letter-spacing: 0.15px;
+  color: #8e8e93;
+  text-align: left;
+  margin-bottom: 24px;
+}
+.cards-container {
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  place-items: center;
+  row-gap: 24px;
+}
+</style>
